21b3d058f20e3947a2fef1a28e5d4b6e1a6e7f92
michael777-3333 <131264034+michael777-3333@users.noreply.github.com>
2024-02-01 10:21:00 -0500

    changes in auth verfy see logs cookies

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -103,7 +103,7 @@ export const profile = async (req, res) => {
 };
 
 export const verifyToken = async (req, res) => {
-  console.log(req);
+  console.log(req.cookies, 'll;;');
   const  token  = req.cookies.token;
   console.log(token, "kkk");
   try {
